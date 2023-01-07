09edaa117024f720f878cfb00506bca6feb0d8b3
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-01-07 23:08:55 +0300

    updated

--- db/schema.rb
+++ db/schema.rb
@@ -25,11 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_06_042826) do
   create_table "customers", force: :cascade do |t|
     t.string "name"
     t.string "email"
-<<<<<<< HEAD
-    t.string "password_digest"
-=======
     t.string "password"
->>>>>>> f8ac254 (seeeding and migrations done)
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -38,8 +34,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_06_042826) do
     t.string "From_location"
     t.string "To_location"
     t.integer "Price"
-<<<<<<< HEAD
-=======
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -62,13 +56,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_06_042826) do
   create_table "vehicles", force: :cascade do |t|
     t.string "vehicle_name"
     t.integer "route_id"
-<<<<<<< HEAD
-    t.datetime "departure_time"
-    t.datetime "arrival_time"
-=======
     t.string "departure_time"
     t.string "arrival_time"
->>>>>>> f8ac254 (seeeding and migrations done)
     t.integer "no_of_seats"
     t.integer "sacco_id"
     t.string "image"
