4070a7366563bdeb2da533806ec3a6d6f02b57dd
+ <+>
2023-01-11 11:10:19 +0300

    changes made in the sacco table

--- app/controllers/saccos_controller.rb
+++ app/controllers/saccos_controller.rb
@@ -1,2 +1,55 @@
 class SaccosController < ApplicationController
-end
+  #before_action :set_sacco, only: %i[ show update destroy ]
+
+  # GET /saccos
+  def index
+    saccos = Sacco.all
+    render json: saccos, status: :ok
+  end
+
+  # GET /saccos/1
+  def show
+    sacco = Sacco.find_by(id: params[:id])
+            if sacco             
+            render json: sacco, status: :created
+            else
+            render json: {error: "Not authorized"}, status: :unauthorized
+            end   
+  end
+
+  # POST /saccos
+  def create
+    sacco = Sacco.create(sacco_params)
+
+    if sacco.valid?
+      render json: sacco, status: :created
+    else
+      render json: {errors: user.error.full_messages} , status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /saccos/1
+   def update
+        sacco=Sacco.find_by(id: params[:id])
+        sacco.update!(sacco_params)
+        render json:sacco,status: :ok        
+      end
+  end
+
+  # DELETE /saccos/1
+    def destroy
+    sacco =Sacco.find_by(id: params[:id])
+    sacco.destroy
+    head :no_content
+    end    
+
+  private
+    # Only allow a list of trusted parameters through.
+    def sacco_params
+      params.permit(:name, :image_url)
+    end
+
+    def render_not_found_response
+       render json: {error:"Sacco not found!"}, status: :not_found
+    end 
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,40 @@
  # seat.booked = false
   #seat.save
 #end
+saccos = Sacco.create([
+
+    {
+    "name": "Modern Coast Sacoo",
+    "email": "moderncoastsacco@gmail.com",
+    "password_digest": "123456"
+    "image_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.glassdoor.com%2FOverview%2FWorking-at-Modern-Coast-EI_IE4343459.11%2C23.htm&psig=AOvVaw2ovcK0537EZRjFGa6PsdPm&ust=1673424556217000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCOjMrLPGvPwCFQAAAAAdAAAAABAS"
+    },
+
+    {
+    "name": "Easy Coach Sacoo",
+    "email": "easycoachsacco@gmail.com",
+    "password_digest": "123456"
+    "image_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Ftwitter.com%2Fisk_kenya%2Fstatus%2F1333399219546288128&psig=AOvVaw119IAfjVpgt8Owjo1XLAY-&ust=1673424774157000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCNCrzJzHvPwCFQAAAAAdAAAAABAD"
+    },
+
+    {
+    "name": "Mololine Sacoo",
+    "email": "mololinesacoo@gmail.com",
+    "password_digest": "123456"
+    "image_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2FMoloLineServicesLimited%2F&psig=AOvVaw0fhVMGgtsASRGQ5-9O6sAg&ust=1673424927320000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCMjX9ubHvPwCFQAAAAAdAAAAABAD"
+    },
+
+    {
+    "name": "Mash Poa Sacoo",
+    "email": "mashpoa@gmail.com",
+    "password_digest": "123456"
+    "image_url": "https://www.google.com/url?sa=i&url=https%3A%2F%2Ftimbu.com%2Fkenya%2Fbus%2Fmash-bus-services-limited%2Fonline-booking&psig=AOvVaw1Xe9LZ9gvWf1_37rlxvc-6&ust=1673425045322000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCMDJmZ7IvPwCFQAAAAAdAAAAABAH"
+    },
+
+    {
+    "name": "Tahmeed Sacoo",
+    "email": "tahmeedsacoo@gmail.com",
+    "password_digest": "123456"
+    "image_url": "https://play.google.com/store/apps/details?id=com.tahmeedcoach&hl=en_US&gl=US"
+    },
+ ])
